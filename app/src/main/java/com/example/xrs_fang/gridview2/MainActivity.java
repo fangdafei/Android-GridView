7190cb3f888bf2be09c278d330fbe35616a45ae3
fangdafei <492103590@qq.com>
2018-08-08 18:13:22 +0800

    修改文字

--- app/src/main/java/com/example/xrs_fang/gridview2/MainActivity.java
+++ app/src/main/java/com/example/xrs_fang/gridview2/MainActivity.java
@@ -17,11 +17,11 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    //定义GridView
+    //定义 GridView
     private GridView gridView;
-    //定义数据
+    //定义 数据
     private List<Map<String, Object>> dataList;
-    //定义适配器
+    //定义 适配器
     private SimpleAdapter adapter;
 
     @Override
